3a024acb57cd3b9b8ace9432caaaa4cf722060f5
Stella Steinfeld <stella.sblanco@gmail.com>
2021-11-21 12:45:13 -0300

    descarga excel

---
+++ src/interfaz/scripts/exportarDatos.js
@@ -0,0 +1,30 @@
+function exportTableToExcel(tableID, filename = ''){
+    var downloadLink;
+    var dataType = 'application/vnd.ms-excel';
+    var tableSelect = document.getElementById(tableID);
+    var tableHTML = tableSelect.outerHTML.replace(/ /g, '%20');
+
+    // Especificar nombre del archivo
+    filename = filename?filename+'.xls':'excel_data.xls';
+
+    // Crear link de descarga
+    downloadLink = document.createElement("a");
+
+    document.body.appendChild(downloadLink);
+
+    if(navigator.msSaveOrOpenBlob){
+        var blob = new Blob(['ufeff', tableHTML], {
+            type: dataType
+        });
+        navigator.msSaveOrOpenBlob( blob, filename);
+    }else{
+        // Crear link al archivo
+        downloadLink.href = 'data:' + dataType + ', ' + tableHTML;
+
+        // Poner nombre al archivo
+        downloadLink.download = filename;
+
+        // llamar a la funcion
+        downloadLink.click();
+    }
+}
